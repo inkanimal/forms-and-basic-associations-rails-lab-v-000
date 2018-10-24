44a730867acbb5f5911439cdc43d4199e0307f36
geoffrey woodcock <theatomproject@gmail.com>
2018-10-24 12:59:43 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,5 +11,13 @@ class Song < ActiveRecord::Base
    def artist_name
       self.artist ? self.artist.name : nil
    end
+   
+     def genre_name=(name)
+     self.genre = Genre.find_or_create_by(name: name)
+   end
+ 
+   def genre_name
+      self.genre ? self.genre.name : nil
+   end
 
 end
